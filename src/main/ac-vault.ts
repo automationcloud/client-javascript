a82a9a99e145f51ae1a9e90a6fcac53e734dba73
Boris Okunskiy <boris@okunskiy.name>
2021-05-28 12:57:24 +0300

    fix: change createDataToken return signature to any

--- src/main/ac-vault.ts
+++ src/main/ac-vault.ts
@@ -60,7 +60,7 @@ export class Vault {
      * Stores specified `data` in Automation Cloud Vault, returning an opaque token
      * that can be used in place of any data inside Job Inputs.
      */
-    async createDataToken(data: any, existingOtp?: string): Promise<object> {
+    async createDataToken(data: any, existingOtp?: string): Promise<any> {
         const otp = existingOtp ?? await this.createOtp();
         const { token } = await this.request.post('/data', {
             body: { otp, data },
